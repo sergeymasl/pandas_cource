d1fb060026ef8843ae8e7af165c6e0eb307c0583
sergeymasl <83965287+sergeymasl@users.noreply.github.com>
2022-08-15 14:31:28 +0300

    Create data_types_and_missing_values.py

---
+++ learntools/data_types_and_missing_values.py
@@ -0,0 +1,13 @@
+# imports
+from core import Tool
+import pandas as pd
+reviews = pd.read_csv('https://drive.google.com/uc?export=download&id=1z-1idT4mGbOvHgmEPzneqV54EJ1-w7tk', index_col = 0)
+
+
+#1
+name_var = 'median_points'
+var = reviews['points'].median()
+what = 'single_value'
+hint = None
+solution = None
+q1 = Tool(name = name_var, what = what, object = var, hint = hint, solution = solution)

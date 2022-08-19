70223be49e949396b037fac5d07e33571f6d0cb8
sergeymasl <83965287+sergeymasl@users.noreply.github.com>
2022-08-19 11:03:25 +0300

    Update renaming_and_combining.py

--- learntools/renaming_and_combining.py
+++ learntools/renaming_and_combining.py
@@ -6,7 +6,8 @@ games_products = pd.read_csv('https://drive.google.com/uc?export=download&id=1Dc
 games_products['subreddit'] = 'r/gaming'
 movies_products = pd.read_csv('https://drive.google.com/uc?export=download&id=1otcaCQ_6cmVHB0HBupsqHdyCqEfkm7Gi')
 movies_products['subreddit'] = 'r/movies'
-
+meets = pd.read_csv('https://drive.google.com/uc?export=download&id=1oOJXjdewC6YKBP-CAn31xp-oaMk5n1kT')
+openpowerlifting = pd.read_csv('https://drive.google.com/uc?export=download&id=1FFvQ59mt0KtHqbc04g8iEl6PWToXDfGN')
 
 #1
 name_var = 'renamed'
@@ -23,3 +24,11 @@ what = 'df'
 hint = "Используйте метод `gf`concat()`n`"
 solution = "combined_products = pd.concat([games_products, movies_products])"
 q2 = Tool(name = name_var, what = what, object = var, hint = hint, solution = solution)
+
+#3
+name_var = 'powerlifting_joined'
+var = openpowerlifting.merge(meets, how = 'inner', on = 'MeetID')
+what = 'df'
+hint = "Используйте метод `gf`df.merge()`n`"
+solution = "powerlifting_joined= openpowerlifting.merge(meets, how = 'inner', on = 'MeetID')"
+q3 = Tool(name = name_var, what = what, object = var, hint = hint, solution = solution)

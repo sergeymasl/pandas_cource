e3651b0b3b509fd581ea9bea188f203212e81b24
sergeymasl <83965287+sergeymasl@users.noreply.github.com>
2022-08-19 09:56:33 +0300

    Update renaming_and_combining.py

--- learntools/renaming_and_combining.py
+++ learntools/renaming_and_combining.py
@@ -2,6 +2,10 @@
 from core import Tool
 import pandas as pd
 reviews = pd.read_csv('https://drive.google.com/uc?export=download&id=1z-1idT4mGbOvHgmEPzneqV54EJ1-w7tk', index_col = 0)
+games_products = pd.read_csv('https://drive.google.com/uc?export=download&id=1Dcm3D9ZllWpgtPpkb_hh-C-pLvE9W7uV')
+games_products['subreddit'] = 'r/gaming'
+movies_products = pd.read_csv('https://drive.google.com/uc?export=download&id=1otcaCQ_6cmVHB0HBupsqHdyCqEfkm7Gi')
+movies_products['subreddit'] = 'r/movies'
 
 
 #1
@@ -13,9 +17,9 @@ solution = "renamed = reviews.rename(columns = {'region_1' : 'region', 'region_2
 q1 = Tool(name = name_var, what = what, object = var, hint = hint, solution = solution)
 
 #2
-name_var = 'renamed'
-var = reviews.rename(columns = {'region_1' : 'region', 'region_2' : 'locale'})
+name_var = 'combined_products'
+var = pd.concat([games_products, movies_products])
 what = 'df'
-hint = "Используйте метод `gf`rename()`n`"
-solution = "renamed = reviews.rename(columns = {'region_1' : 'region', 'region_2' : 'locale'})"
+hint = "Используйте метод `gf`concat()`n`"
+solution = "combined_products = pd.concat([games_products, movies_products])"
 q2 = Tool(name = name_var, what = what, object = var, hint = hint, solution = solution)
